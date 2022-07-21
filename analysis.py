3a29dc8b43a39476c8608f65c2e30e0c101edcd5
AlexV97 <averville1@gmail.com>
2022-07-21 14:34:18 -0400

    read csv successful, added ECR, and formatted tables

--- analysis.py
+++ analysis.py
@@ -1,23 +1,38 @@
 import pandas as pd
+import datetime
 import dateutil
 import string as str
 
-read_addsToCart = pd.read_csv(r'DataAnalyst_Ecom_data_addsToCart.csv')
-read_ecomDataSessionCounts = pd.read_csv(r'DataAnalyst_Ecom_data_sessionCounts.csv')
+read_addsToCart = pd.read_csv(r'DataAnalyst_Ecom_data_addsToCart.csv', header=0)
+read_ecomDataSessionCounts = pd.read_csv(r'DataAnalyst_Ecom_data_sessionCounts.csv', header=0)
 
-print("read_addsToCart_csv = ", read_addsToCart)
+read_ecomDataSessionCounts['ECR'] = read_ecomDataSessionCounts['transactions']/read_ecomDataSessionCounts['sessions']
+
+print("read_addsToCart = ", read_addsToCart)
 print("")
 
-print("read_ecomDataSessionCounts_csv = ", read_ecomDataSessionCounts)
+print("read_ecomDataSessionCounts = ", read_ecomDataSessionCounts)
 print("")
+
+
 
 # Convert data from string to date
-read_ecomDataSessionCounts['dim_date'] = read_ecomDataSessionCounts['dim_date']
+read_ecomDataSessionCounts['dim_date'] = read_ecomDataSessionCounts['dim_date'].astype('datetime64[ns]')
 
 read_ecomDataSessionCounts.index = pd.to_datetime(read_ecomDataSessionCounts['dim_date'], format='%m/%d/%y')
-#read_ecomDataSessionCounts.groupby(by=[read_ecomDataSessionCounts.index.year, read_ecomDataSessionCounts.index.month])
-read_ecomDataSessionCounts.groupby(pd.Grouper(freq='M')).sum()
 
-print("final read_ecomDataSessionCounts_csv = ", read_ecomDataSessionCounts)
+# Cumulating transactions per month per device type
+ecomDataSessionCounts_byMonth = read_ecomDataSessionCounts\
+    .groupby([read_ecomDataSessionCounts['dim_date'].dt.year, read_ecomDataSessionCounts['dim_date'].dt.month, read_ecomDataSessionCounts['dim_deviceCategory']])\
+    ['dim_deviceCategory'].count()
+
+
+
+
+
+print("final read_ecomDataSessionCounts = ", read_ecomDataSessionCounts)
+print("")
+
+print("final ecomDataSessionCounts_byMonth = ", ecomDataSessionCounts_byMonth)
 print("")
 
